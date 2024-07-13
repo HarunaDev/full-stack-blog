9ffda30d2948132c33395fcd0e76d3231c8125e2
Alvin Haruna <harunaalvin69@gmail.com>
2024-07-13 16:13:40 +0100

    linked views to different url paths

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from api.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/user/register/', CreateUserView.as_view(), name="register"), #create user view when that path is visited
+    path('api/token/', TokenObtainPairView.as_view(), name="get_token"), #link token pair view to path
+    path('api/token/refresh/', TokenRefreshView.as_view(), name="refresh"), #link token refresh view to path
+    path('api-auth/', include("rest_framework.urls")), #link all rest frame work urls
+
 ]
