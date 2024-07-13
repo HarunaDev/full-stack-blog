af17fedd5ce4fcdf92721302cdd4dbeb010d7b35
Alvin Haruna <harunaalvin69@gmail.com>
2024-07-13 17:41:01 +0100

    created serializer for artcle model

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Article
 
 # Use serializer to help create user and map the data that a user needs based on the request made
+
+# Create searializer for article 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -13,3 +16,10 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+    
+# Create serializer for article
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model: Article
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}} #it automatically sets who the author is based on the user
