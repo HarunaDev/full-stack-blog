ce53a8b9147474ae987d7a3204849bb63f071698
Alvin Haruna <harunaalvin69@gmail.com>
2024-07-13 17:33:59 +0100

    created models for various fields for each artcle

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Article(models.Model):
+    # create fields for title, content and time created
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    # links author to the user and whenever the user is being deleted all of the notes will be deleted
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="story")
+
+    def __str__(self):
+        return self.title
