e363148ff5bc67b8c12ce9f7e1661e7f0f0a1222
Alvin Haruna <harunaalvin69@gmail.com>
2024-07-14 08:40:16 +0100

    added class for creating an article view with some specific functions

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, ArticleSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Article
 
 # Create your views here.
+
+# make views for creating and deletig an article, using list create view to help list all article or create a new one if none exist
+class ArticleListCreate(generics.ListCreateAPIView):
+    serializer_class = ArticleSerializer #specify serializer class
+    permission_classes = [IsAuthenticated] #allow only authenticated users to access this view
+
+    # get articles written by a user
+    def get_queryset(self):
+        user = self.request.user #set user to the authenticated user
+        return Article.objects.filter(author=user) #gives us all the articles written by this user, you can use all() to get all the articles
+    
+    # specify custom configurations when creating a new articles
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+
 # implement creating a new user view
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all() #view all existing users to prevent creating a user that already exists
