0189b0ece258447fc8e89ea1a7161e82099c3a1d
Alvin Haruna <harunaalvin69@gmail.com>
2024-07-14 19:20:19 +0100

    added code to check local storage for access token

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -8,3 +8,21 @@ import { ACCESS_TOKEN } from "./constants"
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL //import anything specified inside of an environment variable
 })
+
+// interceptor function
+api.interceptors.request.use(
+    // check local storage if it contains access token and adds it to authorization headers to request
+    (config) => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}` //pass jwt access token
+        }
+        return config
+    },
+    // to handle errors if access was not successful
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
+export default api
