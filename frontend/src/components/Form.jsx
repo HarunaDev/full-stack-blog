3828a9ab6d14a9ea2f021e580fd1d49127a53016
Alvin Haruna <harunaalvin69@gmail.com>
2024-07-15 18:04:50 +0100

    added form using material tailwind

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import api from "../api";
 import { useNavigate } from "react-router-dom";
+// import { TextField } from '@material-ui/core'
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 
 import {
@@ -14,45 +15,57 @@ import {
   } from "@material-tailwind/react";
 
   export default function Form({route, method}) {
+    // Store username and password in state and keep track of loading status
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [loading, setLoading] = useState(false)
+
+    // set form title
+    const title = method === 'login' ? 'Login' : 'Register'
+
+    // call useNavigate hook
+    const navigate = useNavigate()
+
+    // handle submit function
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
        <Card color="transparent" shadow={false}>
         <Typography variant="h4" color="blue-gray">
-          Sign Up
+        {/* Display Login or register depending on what method was used */}
+        {title}
         </Typography>
-        <Typography color="gray" className="mt-1 font-normal">
-          Nice to meet you! Enter your details to register.
-        </Typography>
-        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+        
+        <form onSubmit={handleSubmit} className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
           <div className="mb-1 flex flex-col gap-6">
+
+            {/* Name Input */}
             <Typography variant="h6" color="blue-gray" className="-mb-3">
               Your Name
             </Typography>
             <Input
               size="lg"
-              placeholder="name@mail.com"
+              placeholder="enter your name"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
               labelProps={{
                 className: "before:content-none after:content-none",
               }}
             />
-            <Typography variant="h6" color="blue-gray" className="-mb-3">
-              Your Email
-            </Typography>
-            <Input
-              size="lg"
-              placeholder="name@mail.com"
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-              labelProps={{
-                className: "before:content-none after:content-none",
-              }}
-            />
+            
+            {/* Password input */}
             <Typography variant="h6" color="blue-gray" className="-mb-3">
               Password
             </Typography>
             <Input
               type="password"
               size="lg"
-              placeholder="********"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
               labelProps={{
                 className: "before:content-none after:content-none",
@@ -77,8 +90,8 @@ import {
             }
             containerProps={{ className: "-ml-2.5" }}
           />
-          <Button className="mt-6" fullWidth>
-            sign up
+          <Button type="submit" className="mt-6" fullWidth>
+            {title}
           </Button>
           <Typography color="gray" className="mt-4 text-center font-normal">
             Already have an account?{" "}
