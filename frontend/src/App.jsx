24f37f48f6c31f50159723aa107e9b03dd03f5eb
Alvin Haruna <harunaalvin69@gmail.com>
2024-07-15 12:55:33 +0100

    modified root app to import pages and packages, and wrote logout and register function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,23 @@
+/* eslint-disable no-unused-vars */
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import NotFound from "./pages/NotFound"
+import Home from "./pages/Home"
+import ProtectedRouter from "./components/ProtectedRoute"
+
+// write logout function
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+// write register and logout function
+function RegisterAndLogout(){
+  localStorage.clear()
+  return <Register />
+}
+
 function App() {
 
   return (
