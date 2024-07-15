5e1d52dee82547b3005cb26e448247322af9ca70
Alvin Haruna <harunaalvin69@gmail.com>
2024-07-15 18:43:10 +0100

    fixed issues that was causing bug

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -4,7 +4,7 @@ function Register(){
     return (
         <>
             <div className="flex items-center justify-center min-h-screen">
-            <Form route='/api/user/register' method='register' />
+            <Form route='/api/user/register/' method='register' />
             </div>                   
         </>
     )
