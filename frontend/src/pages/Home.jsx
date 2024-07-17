45cecd614e9a30f606ca6f4875c012cdb61eb631
Alvin Haruna <harunaalvin69@gmail.com>
2024-07-17 11:35:47 +0100

    added code to make api call to get articles from backend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,23 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react"
+import api from "../api"
+
 function Home(){
+    // send auth request to get all the articles written, store it in an array with useState
+    const [article, setArticle] = useState([]);
+
+    // store content from form for creating new article in useState
+    const [content, setContent] = useState("")
+    const [title, setTitle] = useState("")
+
+    // function to get articles
+    const getArticles = () => {
+        api
+        .get("/api/notes/")
+        .then((res) => res.data)
+        .then((data) => setArticle(data))
+    }
+
     return (
         <>
             <h1>Welcome to home</h1>            
