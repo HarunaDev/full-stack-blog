4189a60a5a674a320204cb044453ff6056712ac5
Alvin Haruna <harunaalvin69@gmail.com>
2024-08-03 13:01:50 +0100

    fixed bug preventing new user from creating account

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,6 +4,9 @@ from rest_framework import generics
 from .serializers import UserSerializer, ArticleSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Article
+# from rest_framework.response import Response
+# from rest_framework.decorators import api_view, permission_classes
+
 
 # Create your views here.
 
@@ -39,3 +42,10 @@ class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all() #view all existing users to prevent creating a user that already exists
     serializer_class = UserSerializer #specify what type of data to accept to make a new user
     permission_classes =[AllowAny] #allows anyone who is not authenticated to use this view to create a new user
+
+
+# @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+# def get_author(request):
+#     user = request.user
+#     return Response({'username': user.username})
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,10 +12,12 @@ function Home(){
     // store content from form for creating new article in useState
     const [content, setContent] = useState("")
     const [title, setTitle] = useState("")
+    // const [user, setUser] = useState(null)
 
     // put call getArticles inside of a useEffect hook
     useEffect(() => {
         getArticles()
+        // getUser()
     }, [])
 
     // function to get articles
@@ -26,6 +28,17 @@ function Home(){
         .then((data) => {setArticles(data); console.log(data)})
         .catch((err) => alert(err));
     }
+
+    // const getUser = () => {
+    //   api
+    //   .get("/api/user/")
+    //   .then((res) => res.data)
+    //   .then((data) => {
+    //       setUser(data.username);
+    //       console.log(data);
+    //   })
+    //   .catch((err) => alert(err));
+    // };
 
     // function to delete an article
     const deleteArticle = (id) => {
@@ -55,7 +68,7 @@ function Home(){
 
     return (
         <>
-        <h1>Welcome to home</h1>
+        <h1>Welcome hser</h1>
 
         {articles.map((article) => <Article article={article} onDelete={deleteArticle} key={article.id} />)}
 
